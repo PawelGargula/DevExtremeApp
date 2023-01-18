7770c0cc99eef1a2603777f48b050092ebdfb76d
Paweł Gargula <pawel.gargula@interia.pl>
2023-01-18 20:51:10 +0100

    Add notify when user try to select blocked position

--- MvcMovie/wwwroot/js/dropdownBoxWithTreeList.js
+++ MvcMovie/wwwroot/js/dropdownBoxWithTreeList.js
@@ -10,6 +10,8 @@
                 cellTemplate(container, options) {
                     const displayValue = `${options.data.Code} - ${options.value}`;
                     const arr = [2, 3, 4, 5, 6, 11];
+                    const tableRow = container[0].parentElement.parentElement;
+                    tableRow.dataset.id = options.data.Id;
                     if (arr.includes(options.data.Id)) {
                         container.append($('<span>', {
                             text: displayValue
@@ -17,9 +19,9 @@
                     } else {
                         container.append($('<span>', {
                             style: 'color: #EBEBE4;',
-                            title: 'Zablokowana pozycja',
                             text: displayValue
                         }));
+                        tableRow.title = 'Zablokowana pozycja';
                     }
                 }
             }
@@ -54,9 +56,29 @@
             if (!data) return;
             if (arr.includes(keys[0])) {
                 e.component.option('value', keys);
-                e.component.option('inputAttr', { title: selectedItems.selectedRowsData[0].Name });
+                e.component.option('inputAttr', { title: data.Name });
                 e.component.close();
                 e.component.focus();
+            } else {
+                DevExpress.ui.notify(
+                    {
+                        message: 'Nie można wybrać zablokowanej pozycji',
+                        width: 'fit-content',
+                        position: {
+                            my: 'bottom',
+                            at: 'bottom',
+                            of: `#${treeListid}`
+                        }
+                    },
+                    'warning',
+                    1000
+                );
+
+                const element = selectedItems.element[0];
+                const currentSelectedRowKey = selectedItems.currentSelectedRowKeys[0];
+                const currentSelectedRow = element.querySelector(`tr[data-id="${currentSelectedRowKey}"]`);
+                currentSelectedRow.classList.remove('dx-selection');
+                currentSelectedRow.ariaSelected = 'false';
             }
         },
         wordWrapEnabled: true
