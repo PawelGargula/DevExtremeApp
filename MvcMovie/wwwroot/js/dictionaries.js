394c5e52dba23b375d78a0ca1ada79aa5b54ad30
Paweł Gargula <pawel.gargula@interia.pl>
2023-01-21 14:16:11 +0100

    Add css and js files for "Dictionaries" view

---
+++ MvcMovie/wwwroot/js/dictionaries.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    focusSearchPanel();
+});
+
+function focusSearchPanel() {
+    const searachPanel = document.querySelector(".dx-datagrid-search-panel input");
+    searachPanel.focus();
+}
+
+function isEditButtonVisible(item) {
+    const hasPermision = '@hasPermision' === 'True' ? true : false;
+    return item.row.data.IsSystemic || !hasPermision;
+}
+
+function isDeleteButtonVisible(item) {
+    return item.row.data.IsSystemic ? false : true;
+}
+
+function onRowInserted(item) {
+    const key = item.key;
+    const element = item.element[0];
+    const rowEditButton = element.querySelector(`a[data-id="${key}"]`);
+    rowEditButton.focus();
+}
+
+function onRowUpdated() {
+    focusSearchPanel();
+}
+
+function onRowRemoved() {
+    focusSearchPanel();
+}
