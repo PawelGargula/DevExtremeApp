e2139bb20017a8666d6a1c1218e1ab2467cefddf
Paweł Gargula <pawel.gargula@interia.pl>
2023-09-10 14:15:16 +0200

    Add SelectBox example

---
+++ MvcMovie/Models/Item.cs
@@ -0,0 +1,59 @@
+﻿namespace MvcMovie.Models;
+
+public abstract class ValueObject
+{
+    protected static bool EqualOperator(ValueObject left, ValueObject right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+        {
+            return false;
+        }
+        return ReferenceEquals(left, right) || left.Equals(right);
+    }
+
+    protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+    // Other utility methods
+}
+
+public class Item : ValueObject
+{
+    public string Code { get; private set; }
+    public string Name { get; private set; }
+
+    public Item(string code, string name)
+    {
+        Code = code;
+        Name = name;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        // Using a yield return statement to return each element one at a time
+        yield return Code;
+        yield return Name;
+    }
+}
